73a6ceb05fae712bca983e069510585721c1bada
af0liveira <af0liveira@icloud.com>
2023-10-17 14:42:01 +0200

    Implement main method in App
    The app is now able to request input from the user and print the
    retrieved CEP (ZIP code) obtained from the ViaCEP API.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner cliReader = new Scanner(System.in);
+        CepQuery cepQuery = new CepQuery();
+        String cep;
+        Address address;
+
+        System.out.println("""
+            ********************  
+            Localiza CEP
+            ********************  
+            """);
+
+        System.out.println("Qual CEP deseja consultar?");
+        cep = cliReader.nextLine();
+        cliReader.close();
+
+        address = cepQuery.retrieveAddress(cep);
+
+        System.out.println(address);
+
     }
 }
